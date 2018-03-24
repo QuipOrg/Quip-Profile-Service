265379e8dfdc5ae378642e2ef12974d9d7c7fd17
Matheu DeWitt <msdewitt@bsu.edu>
2018-03-24 16:12:42 -0400

    Project init.

---
+++ src/main/java/com/evilscheme/quipprofileservice/QuipProfileServiceApplication.java
@@ -0,0 +1,12 @@
+package com.evilscheme.quipprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuipProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuipProfileServiceApplication.class, args);
+	}
+}
